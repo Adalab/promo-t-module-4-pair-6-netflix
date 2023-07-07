40a8d700eedc15052f9a690f64ede93709cbfdd1
CrisMuniz <cristinamuniz85@gmail.com>
2023-07-07 11:25:29 +0200

    add content to database

--- data.sql
+++ data.sql
@@ -23,3 +23,33 @@ CREATE TABLE actors (
   country VARCHAR(45) NOT NULL,
   year_birthday INT NULL
 ); 
+
+-- Peliculas
+
+SELECT * FROM movies;
+
+SELECT title, genre FROM movies WHERE year > 1990;
+
+SELECT * FROM movies WHERE category = "Top 10";
+
+UPDATE movies SET year = 1997 WHERE (id = 2);
+
+-- Actores 
+
+SELECT * from actors;
+
+SELECT year_birthday, name FROM actors WHERE year_birthday BETWEEN 1950 and 1960;
+
+SELECT name, lastname FROM actors WHERE country = "Estados Unidos";
+
+-- Usuarios
+SELECT * FROM users;
+
+SELECT plan_details FROM users WHERE plan_details = "Standard";
+
+DELETE FROM users WHERE name LIKE "M%" LIMIT 1;
+
+-- Agregar mas campos
+SELECT * from actors;
+
+ALTER TABLE actors ADD image VARCHAR(1000);
