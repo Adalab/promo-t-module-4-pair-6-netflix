020d79a7ff86e6ae8495614d548d8b6e0c23729b
CrisMuniz <cristinamuniz85@gmail.com>
2023-07-12 11:29:51 +0200

    added server

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2/promise');
 
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
 
+async function getConnection() {
+  const connection = await mysql.createConnection(
+    {
+      host: "localhost",
+      user: "root",
+      password: "snout-jitters-unbend",
+      database: "Netflix",
+    }
+  );
+
+  connection.connect();
+
+  return connection;
+}
+
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+
+
+// endpoints
+server.get('/movies', async (req, res) => {
+  const selectMovies = "SELECT * FROM movies";
+  const connection = await getConnection();
+  const [results] = await connection.query(selectMovies);
+  console.log(results);
+  connection.end();
+  res.json(results);
 });
--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -3,7 +3,7 @@
 const getMoviesFromApi = () => {
   console.log('Se están pidiendo las películas de la app');
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json')
+  return fetch('/movies')
     .then(response => response.json())
     .then(() => {
       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
@@ -27,6 +27,7 @@ const getMoviesFromApi = () => {
         ]
       };
     });
+ 
 };
 
 const objToExport = {
